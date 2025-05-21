0704d09288f84150873c9fd835763e6f010e84af
farganzorahealth <fargan.amar@zorahealth.co>
2025-05-21 21:27:57 +0700

    feat: add @vercel/speed-insights dependency and integrate SpeedInsights component

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <SpeedInsights />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
